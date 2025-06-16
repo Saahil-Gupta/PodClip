ae43dbb4bc82449b26a568547ea17319f66244a4
Saahil Gupta <83634729+Saahil-Gupta@users.noreply.github.com>
2025-06-16 16:37:32 +0530

    Basic Front End setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,15 +3,15 @@ import UploadForm from './components/UploadForm.jsx'
 
 export default function App() {
     return ( 
-        <div className="bg-black mx-auto">
-          <div className="px-4 py-8">
+        <div className="bg-gray-500 h-screen w-screen min-h-screen min-w-screen overflow-hidden">
+          <div className="py-15">
             <h1 className="text-white text-2xl text-center text-bold p-5">Welcome to Pod Clip!!</h1>
             <h2 className="text-white text-xl text-center p-5">Your one stop solution for clipping podcasts</h2>
             <p className="text-amber-300 text-center p-3">Upload your locally downloaded Podcast file below</p>
-            <div className="p-3 bg-grey rounded-lg shadow-md">
+            <div className="mx-auto max-w-sm items-center bg-grey rounded-lg bg-gray-400">
               <UploadForm />
             </div>
-          </div>
+          </div>  
         </div>
     );
 }
--- frontend/src/components/UploadForm.jsx
+++ frontend/src/components/UploadForm.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 export default function UploadForm() {
     return ( 
-        <div className="flex justify-items-center p-3">
+        <div className="flex p-3">
             <form className="flex flex-col items-center">
-                <input type="file" accept=".mp3, .wav, .m4a" className="mb-3 p-2 border border-gray-300 rounded" />
-                <button type="submit" className="bg-blue-200 text-amber-200 px-4 py-2 rounded hover:bg-blue-200">Upload</button>
+                <input type="file" accept=".mp3, .wav, .m4a" className="mb-4 inline border border-gray-300 rounded items-center text-white" />
+                <button type="submit" className="bg-gray-500 text-amber-200 px-4 py-2 rounded inline hover:bg-blue-500">Upload</button>
             </form>
         </div>
     );
